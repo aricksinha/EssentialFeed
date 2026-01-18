e328f194e7f8cab5fa7475ddd41677de4ee59362
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-17 21:28:39 -0800

    CreateEssentialFeed framework and added FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Aritra on 17/01/26.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
