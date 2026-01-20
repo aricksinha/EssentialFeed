d1e70a407eba8fc957e6ffae7338ed324372bb05
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-19 20:44:04 -0800

    [MODULE-2][NETWORKING]-Test for request data from URL on load using singleton HTTPClient

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,10 +8,14 @@
 import XCTest
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestedURL: URL?
 }
 
@@ -20,10 +24,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
     // we don't have the URL for api call - lets see the request from another angle i.e if we don't call load() , client.requestedURL is not requested
     // Lets add the client we mentioned
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    // this is load items command in Usecase image
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+        
     }
 
 }
