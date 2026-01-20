e6c3ea874895051d791bc9de1f5dd35b2e6e4fef
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-19 21:59:18 -0800

    [MODULE-2][NETWORKING-6]-Refactor test to remove duplication

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -24,35 +24,37 @@ protocol HTTPClient {
     func get(from url: URL?)
 }
 
-// Only for test
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL?) {
-        requestedURL = url
-    }
-}
-
 final class RemoteFeedLoaderTests: XCTestCase {
 
     // we don't have the URL for api call - lets see the request from another angle i.e if we don't call load() , client.requestedURL is not requested
     // Lets add the client we mentioned
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url,client: client)
-        
+        let (sut, client) = makeSUT()
         XCTAssertNil(client.requestedURL)
     }
     
     // this is load items command in Usecase image
     func test_load_requestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(
+            url: URL(string: "https://a-url.com")!
+        )
         sut.load()
-        XCTAssertEqual(client.requestedURL, url)
-        
+        XCTAssertEqual(client.requestedURL, URL(string: "https://a-url.com"))
     }
 
+    //MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    // Only for test
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL?) {
+            requestedURL = url
+        }
+    }
 }
