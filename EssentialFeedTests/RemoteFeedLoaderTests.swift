8f14ef5fc62a597b436ab1f8ead4fae327ccab4e
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-19 21:11:20 -0800

    [MODULE-2][NETWORKING]-Move Test logic to seperate type HTTPClientSpy

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,14 +9,22 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com"))
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
+    func get(from url: URL?) {}
+}
+
+// Only for test
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL?) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
@@ -24,7 +32,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
     // we don't have the URL for api call - lets see the request from another angle i.e if we don't call load() , client.requestedURL is not requested
     // Lets add the client we mentioned
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
@@ -32,7 +41,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     // this is load items command in Usecase image
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         sut.load()
         XCTAssertNotNil(client.requestedURL)
