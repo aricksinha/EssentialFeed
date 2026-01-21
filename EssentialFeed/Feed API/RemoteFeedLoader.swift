410f9b3abd8555b1f34b465f28fb0be918e1a766
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-21 07:06:17 -0800

    [MODULE-2][NETWORKING-7]-Move remote Feed loader components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,30 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Aritra on 21/01/26.
+//
+
+import Foundation
+
+/// HTTPClient is public coz it can be implemented by external modules
+public protocol HTTPClient {
+    func get(from url: URL?)
+}
+
+/// RemoteFeedLoader is public coz it can be implemented by external modules & it can even be created by another module so make init() also public
+/// load() is behaviour of this class - tested outside so make it public
+/// properties let it be private - noone is using it outside this class
+/// N o use case of subclassing it-
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
