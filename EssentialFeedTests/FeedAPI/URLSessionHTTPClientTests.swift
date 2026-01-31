814730f723e49269660dbcd02c803794dba90089
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-01-31 13:15:19 -0800

    [MODULE-2][NETWORKING-61]-Delivers data and response on successful HTTPURLRequest

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -19,9 +19,11 @@ class URLSessionHTTPClient {
         from url: URL,
         completion: @escaping (HTTPClientResult) -> Void
     ) {
-        session.dataTask(with: url) { _, _, error in
+        session.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
+            } else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
             } else {
                 completion(.failure(UnexpectedValuesRepresentationError()))
             }
@@ -82,6 +84,25 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertNotNil(resultErrorFor(data: anyData(), response: anyHTTPURLResponse(), error: anyNSError()))
         /// - **Case- 9** data: value, response: URLResponse, error:nil
         XCTAssertNotNil(resultErrorFor(data: anyData(), response: nonHTTPURLResponse(), error: nil))
+    }
+    
+    func test_getFromURL_suceedsOnHTTPURLResponseWithData() {
+        let data = anyData()
+        let response = anyHTTPURLResponse()
+        URLProtocolStub.stub(data: data, response: response, error: nil)
+        let exp = expectation(description: "Wait for completion")
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case let .success(receivedData, receivedHTTPURLResponse):
+                XCTAssertEqual(receivedData, data)
+                XCTAssertEqual(receivedHTTPURLResponse.url, response.url)
+                XCTAssertEqual(receivedHTTPURLResponse.statusCode, response.statusCode)
+            default:
+                XCTFail("Expected success, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
     
     //MARK: - Helpers
