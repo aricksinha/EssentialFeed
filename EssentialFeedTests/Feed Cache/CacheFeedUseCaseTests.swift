322f98ad64de6c44531a04493e435f9eb090c4c0
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-02-07 02:39:26 -0800

    [MODULE-3][PERSISTENCE-4]-Save command request cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,23 +6,51 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class FeedStore {
    var  deleteFeedCacheCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteFeedCacheCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
 
-    /// test to check we don't delete the cache upon FeedStore creation
+    /// 1: test to check we don't delete the cache upon FeedStore creation
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteFeedCacheCallCount, 0)
+    }
+    
+    /// 2: test the save command - then request cache deletion
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueFeedItem(), uniqueFeedItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteFeedCacheCallCount, 1)
+    }
+    
+    //MARK: - Helper
+    private func uniqueFeedItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
