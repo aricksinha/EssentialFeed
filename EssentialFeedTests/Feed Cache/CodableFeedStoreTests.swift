bdcac684551d6b268947c9bd2c251c4b49a87bdf
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-02-25 01:59:12 -0800

    [MODULE-3][PERSISTENCE-98]-Create `FeedStoreSpecs` that must be implemented by any `FeedStore` test implementation to guarantee it meets spec

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,39 +8,9 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_HasNoSideEffectOnEmptyCache()
-    func test_retrieve_deliversFoundValueOnNonEmptyCache()
-    func test_retrieve_HasNoSideEffectOnNonEmptyCache()
-    
-    
-    func test_insert_overridePreviousInsertedValue()
-    
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-    
-    func test_storesSideEffect_runsSerially()
-}
+typealias FailableFeedStore = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
 
-/// Interface for error in retrieve()
-protocol FailableRetrieveFeedStoreSpecs {
-    func test_retrieve_deliverFailureOnRetrievalError()
-    func test_retrieve_HasNoSideEffectOnFailure()
-}
-
-/// Interface for error in insert()
-protocol FailableInsertFeedStoreSpecs {
-    func test_insert_deliverOnInsertionError()
-    func test_insert_HasNoSideEffectOnInsertionError()
-}
-
-/// Interface for error in delete
-protocol FailableDeleteFeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_HasNoSideEffectsOnDeletionError()
-}
-final class CodableFeedStoreTests: XCTestCase {
+final class CodableFeedStoreTests: XCTestCase, FailableFeedStore  {
     
     override func setUp() {
         super.setUp()
