997a4a436b59feb025347192c20cfc2c4ed55efd
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-03-01 17:20:17 -0800

    MODULE-3][PERSISTENCE-124]-Extract reusable CoreData helpers into a separate file

--- EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
@@ -6,6 +6,7 @@
 //
 
 import CoreData
+import Foundation
 
 public final class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
@@ -119,33 +120,5 @@ private class ManagedFeedImage: NSManagedObject {
              managedFeedImage.url = localFeed.url
              return managedFeedImage
         })
-    }
-}
-
-private extension NSPersistentContainer {
-    enum LoadingError: Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Swift.Error)
-    }
-    
-    static func load(modelName name: String, url: URL ,bundle: Bundle) throws -> NSPersistentContainer {
-        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-            throw LoadingError.modelNotFound
-        }
-        let container = NSPersistentContainer(name: name, managedObjectModel: model)
-        let description = NSPersistentStoreDescription(url: url)
-        container.persistentStoreDescriptions = [description]
-        var loadError: Error?
-        container.loadPersistentStores { loadError = $1 }
-        try? loadError.map{ throw LoadingError.failedToLoadPersistentStores($0) }
-        return container
-    }
-}
-
-private extension NSManagedObjectModel {
-    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        return bundle
-            .url(forResource: name, withExtension: "momd")
-            .flatMap{ NSManagedObjectModel(contentsOf: $0) }
     }
 }
