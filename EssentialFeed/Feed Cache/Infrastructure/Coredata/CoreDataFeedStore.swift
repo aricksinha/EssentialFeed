783e4049497a0799cb263da986b2105c919a8c99
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-03-01 17:27:53 -0800

    [MODULE-3][PERSISTENCE-126]-Separate CoreData managed model classes data from helpers with extensions

--- EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
@@ -25,6 +25,7 @@ public final class CoreDataFeedStore: FeedStore {
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         perform { context in 
             do {
+                /// find a cache context and if we find it - we delete it then we save the operation
                 try ManagedCache.find(context: context).map(context.delete).map(context.save)
                 completion(nil)
             } catch {
@@ -36,11 +37,13 @@ public final class CoreDataFeedStore: FeedStore {
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
             do {
-                /// Inserting to feed cache
+                /// Get unique instance for the cache in the context
                 let managedCache = try ManagedCache.newUniqueInstances(context: context)
+                // set the timestamp & feed
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, context: context)
                 
+                /// save()
                 try context.save()
                 // nil passed coz no error is there
                 completion(nil)
@@ -54,7 +57,7 @@ public final class CoreDataFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion)  {
         perform { context in
             do {
-                // Fetch Feeds from Coredata cache
+                // Fetch Feeds from Coredata cache - defined a cache in the context(ManagedCache)
                 if let cache = try ManagedCache.find(context: context) {
                     /// Convert cache.feed[NSOrderedSet] into ManagedFeedImage and ManagedFeedImage into LocalFeedImage
                     completion(
@@ -62,6 +65,7 @@ public final class CoreDataFeedStore: FeedStore {
                                timestamp: cache.timestamp)
                     )
                 } else {
+                    /// if can't find the cache
                     completion(.empty)
                 }
             } catch {
