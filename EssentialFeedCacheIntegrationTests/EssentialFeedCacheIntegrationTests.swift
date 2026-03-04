288c20bb3ad2de5ae10f7d1e4cae8c74e53b18c4
Aritra Sinha <37838642+aricksinha@users.noreply.github.com>
2026-03-03 17:15:03 -0800

    MODULE-3][PERSISTENCE-127]-Add `EssentialFeedCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Aritra on 03/03/26.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
