4923d8cf470268eae6a01c2e1713bf20b0cafec4
Aritra Sinha <aritra.sinha@walmart.com>
2026-03-12 16:45:04 +0530

    [Bonus-3]-Simplify `CoreDataFeedStore` completion code with the new `Result` APIs

--- EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/Coredata/CoreDataFeedStore.swift
@@ -23,55 +23,39 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in 
-            do {
+        perform { context in
+            completion(Result {
                 /// find a cache context and if we find it - we delete it then we save the operation
                 try ManagedCache.find(context: context).map(context.delete).map(context.save)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         perform { context in
-            do {
+            completion(Result {
                 /// Get unique instance for the cache in the context
                 let managedCache = try ManagedCache.newUniqueInstances(context: context)
                 // set the timestamp & feed
                 managedCache.timestamp = timestamp
                 managedCache.feed = ManagedFeedImage.images(from: feed, context: context)
-                
+
                 /// save()
                 try context.save()
-                // nil passed coz no error is there
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
-            
+            })
         }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion)  {
         perform { context in
-            do {
+            completion(Result{
                 // Fetch Feeds from Coredata cache - defined a cache in the context(ManagedCache)
-                if let cache = try ManagedCache.find(context: context) {
+                try ManagedCache.find(context: context).map {
                     /// Convert cache.feed[NSOrderedSet] into ManagedFeedImage and ManagedFeedImage into LocalFeedImage
-                    completion(
-                        .success(CachedFeed(feed: cache.localFeeds,
-                                                  timestamp: cache.timestamp))
-                        
-                    )
-                } else {
-                    /// if can't find the cache
-                    completion(.success(.none))
+                    return CachedFeed(feed: $0.localFeeds,
+                                      timestamp: $0.timestamp)
                 }
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
